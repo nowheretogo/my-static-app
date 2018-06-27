2ee4b73c16a85e358bfdf312f14511d913b785c8
Kenji Izui <k.izui@livingstyle.co.jp>
2018-06-27 22:29:27 +0900

    Markdown Example

--- dist/react-static-routes.js
+++ dist/react-static-routes.js
@@ -19,17 +19,25 @@ const universalOptions = {
   },
 }
 
-  
+  const t_0 = universal(import('../src/containers/Home'), universalOptions)
+const t_1 = universal(import('../src/containers/About'), universalOptions)
+const t_2 = universal(import('../src/containers/Blog'), universalOptions)
+const t_3 = universal(import('../src/containers/Post'), universalOptions)
+const t_4 = universal(import('../src/containers/404'), universalOptions)
 
 
 // Template Map
 global.componentsByTemplateID = global.componentsByTemplateID || [
-  
+  t_0,
+t_1,
+t_2,
+t_3,
+t_4
 ]
 
 // Template Tree
 global.templateIDsByPath = global.templateIDsByPath || {
-  '404': undefined
+  '404': 4
 }
 
 // Get template for given path
